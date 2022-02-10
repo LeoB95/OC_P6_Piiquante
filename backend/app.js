25a3c5eba2e92f108587e9203f756388903e825d
Léo Blanchon <leo.blanchon@yahoo.fr>
2022-02-10 09:33:04 +0100

    nouveau backend maj 10.02

---
+++ backend/app.js
@@ -0,0 +1,62 @@
+ 
+const express = require('express');
+
+const app = express();
+
+ 
+module.exports = app;
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+   /* IMPORT MONGODB */ 
+
+const mongoose = require('mongoose'); 
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
+/* CONNEXION A MONGODB DATABASE */ 
+mongoose.connect('mongodb+srv://leo_user_2021:9232092320OC@cluster0.va9ea.mongodb.net/PiquanteDataBase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+   
+  const Thing = require('./models/thing');
+  const stuffRoutes = require('./routes/stuff');
+  app.use('/api/stuff', stuffRoutes);
+  const userRoutes = require('./routes/user');
+  app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
+const path = require('path');
+app.use('/images', express.static(path.join(__dirname, 'images')));
